aa6b18d5d9d5dd3494f7c1c05956ac5b1a64fc98
Damien Jeannerat <damien.jeannerat@unige.ch>
2019-10-15 10:29:34 +0200

    Update lsdnmredata.sh

--- code/lsdnmredata.sh
+++ code/lsdnmredata.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
-echo Converting NMReDATA file to LSD file...
+echo "Reading NMReDATA file to generated LSD file ..."
+echo 'Running: java -cp "*" de.unikoeln.chemie.nmr.ui.cl.LSDConverter ' $1
 java -cp "*" de.unikoeln.chemie.nmr.ui.cl.LSDConverter $1
+echo "Calling Logic for Structure Determination (LSD)"
+echo "Running : lsd " $1.lsd
 ./lsd $1.lsd
+echo "Calling outlsd ..."
 ./outlsd 7 < $1.sol > $1.sol.sdf
-echo Result written to $1.sol.sdf
+echo "Process terminated ..."
+echo "Result in " $1.sol.sdf
