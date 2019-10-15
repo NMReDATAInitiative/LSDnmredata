61acdb337279e4573fd243cbf6e30498cd43e235
Damien Jeannerat <damien.jeannerat@unige.ch>
2019-10-15 10:15:49 +0200

    Update lsdnmredata.sh

--- code/lsdnmredata.sh
+++ code/lsdnmredata.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo Converting NMReDATA file to LSD file...
 java -cp "*" de.unikoeln.chemie.nmr.ui.cl.LSDConverter $1
-lsd $1.lsd
-outlsd 7 < $1.sol > $1.sol.sdf
+./lsd $1.lsd
+./outlsd 7 < $1.sol > $1.sol.sdf
 echo Result written to $1.sol.sdf
